ce2246083352a55dd055bc420425d3977c153b83
unknown <srinifromindia@gmail.com>
2018-04-27 11:17:01 +0530

    newly added few lines

--- src/org/Dummy/Latest.java
+++ src/org/Dummy/Latest.java
@@ -3,6 +3,8 @@ package org.Dummy;
 public class Latest {
 	public static void main(String[] args) {
 		System.out.println("Git first class");
+		System.out.println("Git first class");
+		System.out.println("Git first class");
 	}
 
 }
